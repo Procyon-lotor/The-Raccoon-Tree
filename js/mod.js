4195db4f37ec17a8b12d56e0392456deca427f5c
Procyon-lotor <rsmith27jhu@gmail.com>
2024-11-14 23:11:23 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -6,7 +6,7 @@ let modInfo = {
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (0), // Used for hard resets and new players
+	initialStartPoints: new Decimal (2), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
 }
 
