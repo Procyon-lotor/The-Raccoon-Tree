6e89d89f345e76fdc9ac19d92fa0859aaac007e5
Procyon-lotor <rsmith27jhu@gmail.com>
2024-11-16 19:43:35 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -44,7 +44,7 @@ function getPointGen() {
 	let gain = new Decimal(1)
 	if (hasUpgrade('p', 11)) gain = gain.times(2)
 	if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12))
-	if (hasUpgrade('p', 13)) gain = gain.pow(1.1)
+	if (hasUpgrade('p', 13)) upgradeEffect('p', 12) = upgradeEffect('p', 12).pow(1.1)
 	return gain
 }
 
